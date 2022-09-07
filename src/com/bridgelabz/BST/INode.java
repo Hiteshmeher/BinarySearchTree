eea31ac445fd8f4aa53944726c1fbe77e1fa84c6
Hiteshmeher <meher.hitesh4@gmail.com>
2022-09-07 21:01:40 +0530

    [Hitesh]: added code for creating a BST

---
+++ src/com/bridgelabz/BST/INode.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.BST;
+/**
+ * 
+ * @author LENOVO
+ *
+ */
+public class INode {
+	// initializing
+	int value;
+	INode left;
+	INode right;
+
+	public INode(int value) {
+		//constructors
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
